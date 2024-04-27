a4014db32e82a995cc1972473a98782efb9cecd2
JoshuaMaitland <joshuamaitland1999@outlook.com>
2024-04-27 21:12:16 +0100

    Form1 Preparation Added code from my test win forms project and putting it into this project. I've also added Easter which was not included on my test win forms project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblDaysLeftNewYear = new System.Windows.Forms.Label();
+            this.lblDaysLeftValentines = new System.Windows.Forms.Label();
+            this.lblDaysLeftChristmas = new System.Windows.Forms.Label();
+            this.lblDaysLeftStPatricksDay = new System.Windows.Forms.Label();
+            this.lblDaysLeftHalloween = new System.Windows.Forms.Label();
+            this.lblDaysLeftArmisticeDay = new System.Windows.Forms.Label();
+            this.lblDaysLeftEaster = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDaysLeftNewYear
+            // 
+            this.lblDaysLeftNewYear.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDaysLeftNewYear.ForeColor = System.Drawing.Color.White;
+            this.lblDaysLeftNewYear.Location = new System.Drawing.Point(12, 9);
+            this.lblDaysLeftNewYear.Name = "lblDaysLeftNewYear";
+            this.lblDaysLeftNewYear.Size = new System.Drawing.Size(500, 61);
+            this.lblDaysLeftNewYear.TabIndex = 0;
+            this.lblDaysLeftNewYear.Text = "Days left til New Year: ";
+            this.lblDaysLeftNewYear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDaysLeftValentines
+            // 
+            this.lblDaysLeftValentines.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDaysLeftValentines.ForeColor = System.Drawing.Color.White;
+            this.lblDaysLeftValentines.Location = new System.Drawing.Point(12, 70);
+            this.lblDaysLeftValentines.Name = "lblDaysLeftValentines";
+            this.lblDaysLeftValentines.Size = new System.Drawing.Size(500, 61);
+            this.lblDaysLeftValentines.TabIndex = 1;
+            this.lblDaysLeftValentines.Text = "Days left til Valentines: ";
+            this.lblDaysLeftValentines.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDaysLeftChristmas
+            // 
+            this.lblDaysLeftChristmas.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblDaysLeftChristmas.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDaysLeftChristmas.ForeColor = System.Drawing.Color.White;
+            this.lblDaysLeftChristmas.Location = new System.Drawing.Point(12, 375);
+            this.lblDaysLeftChristmas.Name = "lblDaysLeftChristmas";
+            this.lblDaysLeftChristmas.Size = new System.Drawing.Size(500, 61);
+            this.lblDaysLeftChristmas.TabIndex = 2;
+            this.lblDaysLeftChristmas.Text = "Days left til Christmas: ";
+            this.lblDaysLeftChristmas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDaysLeftStPatricksDay
+            // 
+            this.lblDaysLeftStPatricksDay.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDaysLeftStPatricksDay.ForeColor = System.Drawing.Color.White;
+            this.lblDaysLeftStPatricksDay.Location = new System.Drawing.Point(12, 131);
+            this.lblDaysLeftStPatricksDay.Name = "lblDaysLeftStPatricksDay";
+            this.lblDaysLeftStPatricksDay.Size = new System.Drawing.Size(500, 61);
+            this.lblDaysLeftStPatricksDay.TabIndex = 5;
+            this.lblDaysLeftStPatricksDay.Text = "Days left til St Patrick\'s Day: ";
+            this.lblDaysLeftStPatricksDay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDaysLeftHalloween
+            // 
+            this.lblDaysLeftHalloween.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDaysLeftHalloween.ForeColor = System.Drawing.Color.White;
+            this.lblDaysLeftHalloween.Location = new System.Drawing.Point(12, 253);
+            this.lblDaysLeftHalloween.Name = "lblDaysLeftHalloween";
+            this.lblDaysLeftHalloween.Size = new System.Drawing.Size(500, 61);
+            this.lblDaysLeftHalloween.TabIndex = 6;
+            this.lblDaysLeftHalloween.Text = "Days left til Halloween: ";
+            this.lblDaysLeftHalloween.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDaysLeftArmisticeDay
+            // 
+            this.lblDaysLeftArmisticeDay.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDaysLeftArmisticeDay.ForeColor = System.Drawing.Color.White;
+            this.lblDaysLeftArmisticeDay.Location = new System.Drawing.Point(12, 314);
+            this.lblDaysLeftArmisticeDay.Name = "lblDaysLeftArmisticeDay";
+            this.lblDaysLeftArmisticeDay.Size = new System.Drawing.Size(500, 61);
+            this.lblDaysLeftArmisticeDay.TabIndex = 7;
+            this.lblDaysLeftArmisticeDay.Text = "Days left til Armistice Day: ";
+            this.lblDaysLeftArmisticeDay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDaysLeftEaster
+            // 
+            this.lblDaysLeftEaster.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDaysLeftEaster.ForeColor = System.Drawing.Color.White;
+            this.lblDaysLeftEaster.Location = new System.Drawing.Point(12, 192);
+            this.lblDaysLeftEaster.Name = "lblDaysLeftEaster";
+            this.lblDaysLeftEaster.Size = new System.Drawing.Size(500, 61);
+            this.lblDaysLeftEaster.TabIndex = 8;
+            this.lblDaysLeftEaster.Text = "Days left til Easter: ";
+            this.lblDaysLeftEaster.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(524, 441);
+            this.Controls.Add(this.lblDaysLeftEaster);
+            this.Controls.Add(this.lblDaysLeftArmisticeDay);
+            this.Controls.Add(this.lblDaysLeftHalloween);
+            this.Controls.Add(this.lblDaysLeftStPatricksDay);
+            this.Controls.Add(this.lblDaysLeftChristmas);
+            this.Controls.Add(this.lblDaysLeftValentines);
+            this.Controls.Add(this.lblDaysLeftNewYear);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(540, 39);
+            this.Name = "Form1";
+            this.Text = "World Special Days Left in YEAR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblDaysLeftNewYear;
+        private System.Windows.Forms.Label lblDaysLeftValentines;
+        private System.Windows.Forms.Label lblDaysLeftChristmas;
+        private System.Windows.Forms.Label lblDaysLeftStPatricksDay;
+        private System.Windows.Forms.Label lblDaysLeftHalloween;
+        private System.Windows.Forms.Label lblDaysLeftArmisticeDay;
+        private System.Windows.Forms.Label lblDaysLeftEaster;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+// Other namespace
+using Holidays;
 
 namespace WorldSpecialDaysCountdown
 {
@@ -15,6 +10,68 @@ namespace WorldSpecialDaysCountdown
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "World Special Days Left in " + DateTime.Now.Year;
+#if DEBUG
+            Text += " [DEBUG]";
+#endif
+            DateTime newYearDate = new DateTime(DateTime.Now.Year + 1, 1, 1);
+            DateTime valentinesDate = new DateTime(DateTime.Now.Year, 2, 14);
+            DateTime stPatricksDayDate = new DateTime(DateTime.Now.Year, 3, 17);
+            DateTime easterDate = ChristianHolidays.EasterSunday(DateTime.Now.Year);
+            DateTime halloweenDate = new DateTime(DateTime.Now.Year, 10, 31);
+            DateTime armisticeDate = new DateTime(DateTime.Now.Year, 11, 11);
+            DateTime christmasDate = ChristianHolidays.ChristmasDay(DateTime.Now.Year);
+
+            double daysUntilNewYear = newYearDate.Subtract(DateTime.Today).TotalDays;
+            double daysUntilValentines = valentinesDate.Subtract(DateTime.Today).TotalDays;
+            double daysUntilStPatricksDay = stPatricksDayDate.Subtract(DateTime.Today).TotalDays;
+            double daysUntilEaster = easterDate.Subtract(DateTime.Today).TotalDays;
+            double daysUntilHalloween = halloweenDate.Subtract(DateTime.Today).TotalDays;
+            double daysUntilArmistice = armisticeDate.Subtract(DateTime.Today).TotalDays;
+            double daysUntilChristmas = christmasDate.Subtract(DateTime.Today).TotalDays;
+
+            // Check if it's all over and these events will have to wait until next year
+            if (daysUntilValentines <= 0)
+            {
+                daysUntilValentines = 0;
+            }
+
+            if (daysUntilStPatricksDay <= 0)
+            {
+                daysUntilStPatricksDay = 0;
+            }
+
+            if (daysUntilEaster <= 0)
+            {
+                daysUntilEaster = 0;
+            }
+
+            if (daysUntilHalloween <= 0)
+            {
+                daysUntilHalloween = 0;
+            }
+
+            if (daysUntilArmistice <= 0)
+            {
+                daysUntilArmistice = 0;
+            }
+
+            if (daysUntilChristmas <= 0)
+            {
+                daysUntilChristmas = 0;
+            }
+
+            lblDaysLeftNewYear.Text += daysUntilNewYear.ToString();
+            lblDaysLeftValentines.Text += daysUntilValentines.ToString();
+            lblDaysLeftStPatricksDay.Text += daysUntilStPatricksDay.ToString();
+            lblDaysLeftEaster.Text += daysUntilEaster.ToString();
+            lblDaysLeftHalloween.Text += daysUntilHalloween.ToString();
+            lblDaysLeftArmisticeDay.Text += daysUntilArmistice.ToString();
+            lblDaysLeftChristmas.Text += daysUntilChristmas.ToString();
         }
     }
 }
