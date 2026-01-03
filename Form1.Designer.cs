0f565a3ea5b74e58cabff1a61ac6efa54079506e
JoshuaMaitland <joshuamaitland1999@outlook.com>
2026-01-03 21:15:43 +0000

    Always start the program on a center screen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -158,6 +158,7 @@
             this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(540, 600);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "World Special Days Left in YEAR";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
