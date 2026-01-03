6bf1ff6c295695eead512ae8990b802be1bedf71
JoshuaMaitland <joshuamaitland1999@outlook.com>
2026-01-03 20:27:22 +0000

    Update copyright year I've also added my "own" name on the copyright. This is to show that I made this program.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WorldSpecialDaysCountdown")]
-[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyCopyright("Copyright © 2024-2026 JoshuaMaitland")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
